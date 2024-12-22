d79191fc488e2f004b37a2c4ea0ffe94fe5a48ea
hebotkr <heprabathmdacc@gmail.com>
2024-12-22 10:15:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "6BdEyIrI#gkn6rr2dAb9WnhK3c2DEoZGfx2tRgcICtZJKmUO2mvM", //put your session id
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.pn",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 AUTO_VOICE:"false" //true or false
 };
