5f92953f6ae1f9c6636b564a5d6ffb76ba0c9218
hebotkr <heprabathmdacc@gmail.com>
2024-12-29 13:57:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=TN0mwBaQ#GJruXP_VB8JZ0NT08Dhiv6v0IOewaJlCQoDx3eVG4XY", //put your session id
+SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=6BdEyIrI#gkn6rr2dAb9WnhK3c2DEoZGfx2tRgcICtZJKmUO2mvM ", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.pn",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 AUTO_VOICE:"true" //true or false
